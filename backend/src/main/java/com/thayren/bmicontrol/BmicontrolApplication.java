49b402125ec41a7dd4e877dc269076625d0b6cbc
Renan Duarte <renandg.axl@gmail.com>
2023-01-16 16:02:17 -0300

    Backend project created

---
+++ backend/src/main/java/com/thayren/bmicontrol/BmicontrolApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.bmicontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmicontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmicontrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayren/bmicontrol/BmicontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.bmicontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmicontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

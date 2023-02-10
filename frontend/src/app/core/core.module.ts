644d2dc780461fc503d7e0ae1c83089bfd94b2c7
Renan Duarte <renandg.axl@gmail.com>
2023-02-10 19:26:17 -0300

    Frontend: Setting the locale to pt-BR for values to be in the pt-BR pattern. Example: 0.000,00

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import localePt from '@angular/common/locales/pt';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
-
-
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
+  declarations: [NavbarComponent],
   imports: [
-    CommonModule,
+    CommonModule, 
     RouterModule
   ],
-  exports:[
-    NavbarComponent
+  exports: [NavbarComponent],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ]
 })
-export class CoreModule { }
+export class CoreModule {}

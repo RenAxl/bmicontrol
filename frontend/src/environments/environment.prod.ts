a00274020d0c6e84052f55abfa8aaaa8feb7f95e
Renan Duarte <renandg.axl@gmail.com>
2023-07-26 19:19:02 -0300

    Frontend: fixes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: 'http://ec2-18-231-87-192.sa-east-1.compute.amazonaws.com',
   oauthTokenUrl: 'http://ec2-18-231-87-192.sa-east-1.compute.amazonaws.com/oauth/token',
-  tokenAllowedDomains: 'localhost:8080',
-  tokenDisallowedRoutes: 'http://localhost:8080/oauth/token'
+  tokenAllowedDomains: 'ec2-18-231-87-192.sa-east-1.compute.amazonaws.com',
+  tokenDisallowedRoutes: 'http://ec2-18-231-87-192.sa-east-1.compute.amazonaws.com/oauth/token'
 };
